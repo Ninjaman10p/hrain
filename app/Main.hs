0dedf5ca7ac433d0c4e5b6849a8d4746f15c0e05
Peter Gow <petergow@live.com.au>
2022-08-14 13:18:05 +1000

    Added help message

--- app/Main.hs
+++ app/Main.hs
@@ -4,14 +4,16 @@ module Main where
 
 import Sim
 import Presets
-import System.Environment (getArgs)
+import System.Environment (getArgs, getProgName)
 import qualified Data.Map as M
 
 main :: IO ()
 main = do
   args <- getArgs
+  progname <- getProgName
   case args of
     []    -> mkSim rainSim
+    ("--help":_) -> putStrLn $ helpMessage progname
     (a:_) -> case M.lookup a sims of
       (Just sim) -> mkSim sim
       Nothing    -> putStrLn "ERROR: Simulation not found"
@@ -29,3 +31,12 @@ sims = M.fromList
   , ("knife", knifeSim)
   , ("thanks-fish", dolphinSim)
   ]
+  
+helpMessage :: String -> String
+helpMessage progname = unlines $
+  [ "USAGE: " <> progname <> " [SIMULATION]"
+  , "       " <> progname <> " --help (to display this message)"
+  , ""
+  , "SIMULATIONS: (default: rain)"
+  ] <> (("    " <>) <$> M.keys sims)
+
